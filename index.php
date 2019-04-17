8a266c99e200ea87da55470dc76c1f08a96995d4
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-04-17 17:20:37 +0530

    2019 04 17

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ICT Institute</title>
+	<?php include 'head.php'; ?>
+	
+</head>
+<body>
+	
+  <!-- Content here -->
+
+
+
+
+<?php 
+		if (isset($_GET['page'])) {
+			switch ($_GET['page']) {
+				
+				case 'registration':
+					include 'registration.php';
+					break;
+				case 'course':
+					include 'course-registration.php';
+					break;
+				case 'attendance':
+					include 'attendance-course.php';
+					break;
+				case 'payment':
+					include 'payment.php';
+					break;
+				case 'finance':
+					include 'finance.php';
+					break;
+				default:
+					# code...
+					break;
+			}
+		}else{
+			include 'menu.php';
+
+		}
+ ?>
+
+ <?php include 'footer.php';?>
+
+
+
+</body>
+</html>
+
